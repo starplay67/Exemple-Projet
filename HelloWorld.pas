71a2ecf41e951f5064829a70cf83cabcd9431404
S3PORT17\Léo <starplay67@laposte.net>
2017-10-31 15:26:34 +0100

    version 2

--- HelloWorld.pas
+++ HelloWorld.pas
@@ -2,8 +2,15 @@ PROGRAM hello_world;
 
 uses crt;
 
+VAR 
+	prixht : integer;
+
+
 begin;
 	writeln('Hello, World.');
+	writeln('Veuillez entrer un prix hors taxe');
+	readln(prixht);
+	writeln('Voici le prix ttc : ',prixht*1.2);
 	readln;
 
 END.
